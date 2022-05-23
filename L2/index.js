8542af519e0d977d5c21846cf8357489fe74f126
Al Fareed <76606638+Al-Fareed@users.noreply.github.com>
2022-05-23 19:57:03 +0500

    Lab works till (WEEK9)L9
    You can get all the lab programs till L9

---
+++ L2/index.js
@@ -0,0 +1,11 @@
+function display(){
+    var val=document.getElementById("in").value;
+    alert(val);
+    document.getElementById("dis").style.display="none";
+    document.getElementById("is").style.display="block";
+}
+    document.getElementById('name').addEventListener;('keypress', function(event) {
+        if (event.keyCode == 13) {
+            event.preventDefault();
+        }
+    });
